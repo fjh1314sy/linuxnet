0f3567e61f36839a4dec0ff38786b1b876b4a47c
fjh1314sy <35087868+fjh1314sy@users.noreply.github.com>
2018-12-20 23:49:42 +0800

    Update main.c

--- lab3/main.c
+++ lab3/main.c
@@ -180,11 +180,97 @@ int Hello(int argc, char *argv[])
 #include <netinet/in.h>
 #include <net/if.h>
 #include <arpa/inet.h>
- 
+#include <sys/types.h>
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <errno.h> 
+#include <stdio.h> 
+#include <unistd.h> 
+#include <string.h>
+#include <stdlib.h> 
+#include <arpa/inet.h>  
+#define MAX_BUF_SIZE 1024 
 #include <unistd.h>
-#include <stdio.h>
 #define MAX_IFS 64
- 
+
+void udp_msg_sender(int fd, struct sockaddr* dst)
+{
+
+    socklen_t len;
+    struct sockaddr_in src;
+    char buf[BUFF_LEN] = "TEST UDP MSG \n";
+    len = sizeof(*dst);
+    printf("client:%s\n",buf);  //打印自己发送的信息     sendto(fd, buf, BUFF_LEN, 0, dst, len);
+    memset(buf, 0, BUFF_LEN);
+    while(1)
+    {
+        recvfrom(fd, buf, BUFF_LEN, 0, (struct sockaddr*)&src, &len);  //接收来自server的信息         printf("get server msg:%s\n",buf);
+        return 0;
+    }
+}
+
+void handle_udp_msg(int fd)
+{
+    char buf[BUFF_LEN];  //接收缓冲区，1024字节     socklen_t len;
+    int count;
+    struct sockaddr_in clent_addr;  //clent_addr用于记录发送方的地址信息     while(1)
+    {
+        memset(buf, 0, BUFF_LEN);
+        len = sizeof(clent_addr);
+        count = recvfrom(fd, buf, BUFF_LEN, 0, (struct sockaddr*)&clent_addr, &len);  //recvfrom是拥塞函数，没有数据就一直拥塞         if(count == -1)
+        {
+            printf("recieve data fail!\n");
+            return;
+        }
+        printf("get_client_message:%s\n",buf);  //打印client发过来的信息         memset(buf, 0, BUFF_LEN);
+        sprintf(buf, "I have recieved %d bytes data!\n", count);  //回复client         printf("server:%s\n",buf);  //打印自己发送的信息给         sendto(fd, buf, BUFF_LEN, 0, (struct sockaddr*)&clent_addr, len);  //发送信息给client，注意使用了clent_addr结构体指针 
+    }
+}
+
+int  UDPStartReplayhi(){
+    int server_fd, ret;
+    struct sockaddr_in ser_addr; 
+
+    server_fd = socket(AF_INET, SOCK_DGRAM, 0); //AF_INET:IPV4;SOCK_DGRAM:UDP     if(server_fd < 0)
+    {
+        printf("create socket fail!\n");
+        return -1;
+    }
+
+    memset(&ser_addr, 0, sizeof(ser_addr));
+    ser_addr.sin_family = AF_INET;
+    ser_addr.sin_addr.s_addr = htonl(INADDR_ANY); //IP地址，需要进行网络序转换，INADDR_ANY：本地地址     ser_addr.sin_port = htons(SERVER_PORT);  //端口号，需要网络序转换 
+    ret = bind(server_fd, (struct sockaddr*)&ser_addr, sizeof(ser_addr));
+    if(ret < 0)
+    {
+        printf("socket bind fail!\n");
+        return -1;
+    }
+
+    handle_udp_msg(server_fd);   //处理接收到的数据 
+    close(server_fd);
+}
+
+int UDPHello (){
+    int client_fd;
+    struct sockaddr_in ser_addr;
+
+    client_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(client_fd < 0)
+    {
+        printf("create socket fail!\n");
+        return -1;
+    }
+
+    memset(&ser_addr, 0, sizeof(ser_addr));
+    ser_addr.sin_family = AF_INET;
+    //ser_addr.sin_addr.s_addr = inet_addr(SERVER_IP);     ser_addr.sin_addr.s_addr = htonl(INADDR_ANY);  //注意网络序转换     ser_addr.sin_port = htons(SERVER_PORT);  //注意网络序转换 
+    udp_msg_sender(client_fd, (struct sockaddr*)&ser_addr);
+
+    close(client_fd);
+
+}
+
 int BringUpNetInterface()
 {
     printf("Bring up interface:lo\n");
@@ -280,6 +366,8 @@ int main()
     MenuConfig("quit","Quit from MenuOS",Quit);
     MenuConfig("replyhi", "Reply hi TCP Service", StartReplyhi);
     MenuConfig("hello", "Hello TCP Client", Hello);
+    MenuConfig("UDPreplyhi", "Replay hi UDP Service", UDPStartReplayhi);
+    MenuConfig("UDPhello", "Hello UDP Client", UDPHello);
     ExecuteMenu();
 }
 
